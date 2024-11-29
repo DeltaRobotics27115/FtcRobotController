793a3e3c67871327c7834b3262655c92b5d29096
limee <112005874+EnabledLimee@users.noreply.github.com>
2024-11-28 21:16:08 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristAndIntake.java
@@ -0,0 +1,58 @@
+//toilet phonk
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class WristAndIntake extends LinearOpMode {
+    public Blinker control_Hub;
+    public Servo wrist;
+    public CRServo intake;
+
+
+
+    @Override
+    public void runOpMode() {
+        control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
+        wrist = hardwareMap.get(Servo.class, "Wrist");
+        intake = hardwareMap.get(CRServo.class, "Intake");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //wait for game to start
+        waitForStart();
+
+        wrist.setPosition(0.5);
+
+        //run until end
+        while (opModeIsActive()) {
+            //wrist left and right
+            if (gamepad2.dpad_left) {
+                wrist.setPosition(1);
+            } else if (gamepad2.dpad_right) {
+                wrist.setPosition(0);
+            } else {
+                wrist.setPosition(0.5);
+            }
+
+            if (gamepad2.left_bumper) {
+                intake.setPower(1);
+            } else if (gamepad2.right_bumper) {
+                intake.setPower(-1);
+            } else {
+                intake.setPower(0);
+            }
+        }
+    }
+}

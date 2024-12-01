d16f70607bede34c027ab4aabda1b37c01db78b9
BE____________________________________N <benjamin9wang@gmail.com>
2024-12-01 15:47:56 -0500

    Driver Code added by ben

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDRIVE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDRIVE.java
@@ -1,31 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class Drive extends LinearOpMode {
-
+public class FieldCentricDRIVE extends LinearOpMode {
+    //find motors
     public DcMotor frontLeft, frontRight, backLeft, backRight;
-
-
+    //find IMU
+    BHI260IMU imu;
     @Override
     public void runOpMode() {
+        //declare motors
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        //wait for game to start
-        waitForStart();
-        double slowAmount = 0;
+        //reverse direction
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+        //wait for game to start
+        waitForStart();
+        //declare IMU
+        imu = hardwareMap.get(BHI260IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
+        imu.initialize(parameters);
+        imu.resetYaw();
         //run until end
         while (opModeIsActive()) {
+            //get IMU
+            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             //inputs
             double x = gamepad1.left_stick_x;
             double y = -gamepad1.left_stick_y;
@@ -38,10 +51,10 @@ public class Drive extends LinearOpMode {
             double max = Math.max(Math.abs(sin),
                     Math.abs(cos));
             //power calculation
-            double leftFront = power * cos/max + turn;
-            double rightFront = power * sin/max - turn;
-            double leftBack = power * sin/max + turn;
-            double rightBack = power * cos/max - turn;
+            double leftFront = power * cos/max + turn - heading;
+            double rightFront = power * sin/max - turn + heading;
+            double leftBack = power * sin/max + turn - heading;
+            double rightBack = power * cos/max - turn + heading;
             //cut power to 1
             if ((power + Math.abs(turn)) > 1) {
                 leftFront /= power + turn;
@@ -49,17 +62,11 @@ public class Drive extends LinearOpMode {
                 leftBack /= power + turn;
                 rightBack /= power + turn;
             }
-            //slow mode
-            if (gamepad1.right_trigger > 0.1) {
-                slowAmount = 0.3;
-            } else {
-                slowAmount = 0;
-            }
             //run
-            frontLeft.setPower(leftFront - slowAmount);
-            frontRight.setPower(rightFront - slowAmount);
-            backLeft.setPower(leftBack - slowAmount);
-            backRight.setPower(rightBack - slowAmount);
+            frontLeft.setPower(leftFront);
+            frontRight.setPower(rightFront);
+            backLeft.setPower(leftBack);
+            backRight.setPower(rightBack);
         }
     }
 }

19d2a913b39a825fac64cd1f4bd0c99d40b76da6
limee <112005874+EnabledLimee@users.noreply.github.com>
2024-12-08 15:46:23 -0500

    Update and rename Intake.java to WristAndIntake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristAndIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristAndIntake.java
@@ -1,25 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class Intake extends LinearOpMode {
+public class WristAndIntake extends LinearOpMode {
+    public Servo wrist;
     public CRServo intake;
 
     @Override
     public void runOpMode() {
+        wrist = hardwareMap.get(Servo.class, "Wrist");
         intake = hardwareMap.get(CRServo.class, "Intake");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         // Wait for the game to start
         waitForStart();
-        // Run until the end
+        double wristcurrentposition = 0.5;
         while (opModeIsActive()) {
+
+            // Wrist movement
+            if (gamepad2.left_trigger > 0.2) {
+                while (gamepad2.left_trigger > 0.2) {
+                    wristcurrentposition = wristcurrentposition + 0.05;
+                    telemetry.addData("Wrist Status", "Moving Left");
+                    sleep(250);
+                }
+            } else if (gamepad2.right_trigger > 0.2) {
+                while (gamepad2.right_trigger > 0.2) {
+                    wristcurrentposition = wristcurrentposition - 0.05;
+                    telemetry.addData("Wrist Status", "Moving Right");
+                    sleep(250);
+                }
+            }
+            
+            wrist.setPosition(wristcurrentposition);
 
             // Intake control with debugging
             if (gamepad2.left_bumper) {

cc40d239ca18c6e3aa3cb8a8d684b277a8c80e12
benishim12 <benjamin9wang@gmail.com>
2024-12-28 11:15:51 -0500

    Add motors to Autonomous

---
+++ memberCode/src/main/java/com/deltarobotics27115/membercode/Autonomous.java
@@ -0,0 +1,21 @@
+package com.deltarobotics27115.membercode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Autonomous extends LinearOpMode {
+    //find motors
+    public DcMotor frontLeft, frontRight, backLeft, backRight;
+    @Override
+    public void runOpMode() {
+        //declare motors
+        frontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        backRight = hardwareMap.get(DcMotor.class, "BackRight");
+//Go forward until submersible
+//Go right until wall
+//Go left a little bit
+//Go forward a little
+//Turn right then forward
+//Strafe right

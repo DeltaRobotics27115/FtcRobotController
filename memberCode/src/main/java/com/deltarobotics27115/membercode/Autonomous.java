e6699a723d54d21f2ddf8dd32f1f6fb2b144334b
benishim12 <benjamin9wang@gmail.com>
2024-12-28 13:58:35 -0500

    Add timer

--- memberCode/src/main/java/com/deltarobotics27115/membercode/Autonomous.java
+++ memberCode/src/main/java/com/deltarobotics27115/membercode/Autonomous.java
@@ -2,6 +2,7 @@ package com.deltarobotics27115.membercode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Autonomous extends LinearOpMode {
     //find motors
@@ -14,11 +15,22 @@ public class Autonomous extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         backRight = hardwareMap.get(DcMotor.class, "BackRight");
-        //Go forward until submersible
-        //Go right until wall
-        //Go left a little bit
-        //Go forward a little
-        //Turn right then forward
-        //Strafe right
+        //create variables for adjustment
+        double leftFrontValue = 0, leftBackValue = 0, rightFrontValue = 0, rightBackValue = 0;
+        //add timer
+        ElapsedTime timer = new ElapsedTime();
+        waitForStart();
+        while (opModeIsActive()) {
+            frontLeft.setPower(leftFrontValue);
+            backLeft.setPower(leftBackValue);
+            frontRight.setPower(rightFrontValue);
+            backRight.setPower(rightBackValue);
+            //Go forward until submersible
+            //Go right until wall
+            //Go left a little bit
+            //Go forward a little
+            //Turn right then forward
+            //Strafe right
+        }
     }
 }
